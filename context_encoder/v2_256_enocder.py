6cb7e3ae453459530cc902ffddc142940df8ed64
bensantos <santosb@carleton.edu>
2021-01-20 13:43:05 -0700

    label smoothing

--- context_encoder/v2_256_enocder.py
+++ context_encoder/v2_256_enocder.py
@@ -182,8 +182,10 @@ class ContextEncoder():
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
-
+        
         for epoch in range(epochs):
+            valid = valid - (np.random.uniform(0,.05))
+            fake = fake + (np.random.uniform(0,.05))
             tq = tqdm(range(int(len(train_data)/batch_size)), desc=f"Epoch: {epoch}")
             for ind in tq:
                 # ---------------------
