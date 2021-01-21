fa60f96725c244b0254caa4d97c3c8535f428b17
bensantos <santosb@carleton.edu>
2021-01-21 15:49:38 -0700

    Update v2_256_enocder.py

--- context_encoder/v2_256_enocder.py
+++ context_encoder/v2_256_enocder.py
@@ -267,7 +267,7 @@ class ContextEncoder():
 
 
 if __name__ == '__main__':
-    paths = get_image_paths(r"/home/ben/gans_git/auto_encoder_experiments/cocodataset/unlabeled2017")
+    paths = get_image_paths(r"/home/ben/gans_git/Keras-GAN/landscapes")
     context_encoder = ContextEncoder()
     context_encoder.train(paths, epochs= 100, batch_size=256, sample_interval=50, load = False)
     context_encoder.save_model()
